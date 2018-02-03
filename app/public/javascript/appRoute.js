a67576a748728a0536474ebf84d48ef07c4d0d0f
Shany Natan <Shani20100@gmail..com>
2018-02-03 22:53:00 +0200

    no message

--- app/public/javascript/appRoute.js
+++ app/public/javascript/appRoute.js
@@ -1,46 +1,48 @@
 var myApp = angular.module("myApp", ['ngRoute', 'ngCookies']);
 
 myApp.config(function($routeProvider) {
-  $routeProvider.
-  when('/home', {
-      templateUrl: "views/home.html",
-      controller : 'userCtrl'
-  }).
-  when('/login', {
-     templateUrl: 'views/login.html',
-     controller : 'userCtrl'
-  }).
-  when('/register', {
-     templateUrl: 'views/register.html',
-     controller : 'userCtrl'
-  }).
-  when('/branches', {
-    templateUrl: "views/branches.html",
-    controller : 'branchCtrl'
- }).
-  when('/map', {
-      templateUrl: "views/map.html",
-      controller : 'branchCtrl'
-  }).
-  when('/users', {
-     templateUrl: "views/users.html",
-     controller : 'userCtrl'
-  }).
-  when('/transactions', {
-      templateUrl: "views/transactions.html",
-      controller : 'transactionCtrl'
-  }).
-  when('/graph', {
-  when('/piechart', {
+    $routeProvider.
+    when('/home', {
+        templateUrl: "views/home.html",
+        controller : 'userCtrl'
+    }).
+    when('/login', {
+        templateUrl: 'views/login.html',
+        controller : 'userCtrl'
+    }).
+    when('/register', {
+        templateUrl: 'views/register.html',
+        controller : 'userCtrl'
+    }).
+    when('/contact', {
+        templateUrl: "views/map.html",
+        controller : 'branchCtrl'
+    }).
+    when('/currency', {
+        templateUrl: "views/currency.html",
+    }).
+    when('/users', {
+        templateUrl: "views/users.html",
+        controller : 'userCtrl'
+    }).
+    when('/transactions', {
+        templateUrl: "views/transactions.html",
+        controller : 'transactionCtrl'
+    }).
+    when('/piechart', {
         templateUrl: "views/piechart.html",
         controller : 'transactionCtrl'
-  }).
-  when('/statistics', {
-      templateUrl: "views/statistics.html",
-      controller : 'transactionCtrl'
-  }).
-   otherwise({
-     redirectTo: '/home'
-  });
+    }).
+    when('/barchart', {
+        templateUrl: "views/barchart.html",
+        controller : 'transactionCtrl'
+    }).
+    when('/statistics', {
+        templateUrl: "views/statistics.html",
+        controller : 'transactionCtrl'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
 });
 
