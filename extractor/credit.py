8768ec475110f24931b789d6b40eb523329a76e1
shalev67 <shalev67@gmail.com>
2018-03-27 02:39:45 +0300

    Upload pdf login and extractor support for mastercard pdf
    * Changed logo6 to Logo6 url because case sensitive
    
    * handle empty file
    
    * Added transaction by user id, Added cors
    
    * fix typo buissness -> business
    
    * Push only new transactions, push all elements with one mogno command
    
    * handle hebrew character
    
    * Handling different types of pdfs easier
    
    * Added support for mastercard pdf
    
    * upload page requires login

--- extractor/credit.py
+++ extractor/credit.py
@@ -2,9 +2,9 @@ import textract
 from itertools import tee
 import base64
 import bson
+import uuid
 import os
-from flask import Flask, request, make_response
-from werkzeug.utils import secure_filename
+from flask import Flask, request, jsonify
 import datetime
 from pymongo import MongoClient
 
@@ -22,17 +22,30 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.after_request
+def cors_enabled(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5000'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
+    is_request_post = request.method == 'POST'
+    is_user_cookie_string = type(request.cookies.get('currentUserId')) is str
+    is_user_len_ok = type(request.cookies.get('currentUserId')) is str
+    if is_request_post and is_user_cookie_string and is_user_len_ok:
+        user_id = request.cookies.get('currentUserId')[3:-3]
         file_stream = request.form['file']
-        filename = 'data.pdf'
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], 'data.pdf'), 'wb') as file:
+        file_name = uuid.uuid4().hex + '.pdf'
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+        with open(file_path, 'wb') as file:
             file.write(base64.b64decode(file_stream[28:]))
 
-        extract_transaction_from_pdf(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'OK!'
+        extract_transaction_from_pdf(file_path, user_id)
+        return jsonify({"message": "User id : %s transaction was updated" % user_id})
     else:
+        # TODO remove GET
         return 'Hello World!'
 
 
@@ -51,6 +64,7 @@ def remove_rtl(text):
     text = text.replace('ó', 'ף')
     text = text.replace('ê', 'ך')
     text = text.replace('õ', 'ץ')
+    text = text.replace('', 'נ')
     return text
 
 
@@ -86,58 +100,107 @@ class Data:
     transactions = None
 
 
-def extract_transaction_from_pdf(file_path):
-    text = textract.process(file_path, 'UTF-8')
+def add_transaction_to_user(transactions, user_id):
+    current_transactions = users_collection.find_one({'_id': bson.ObjectId(user_id)})['transactions']
+    for transaction in current_transactions:
+        transaction.pop('_id', None)
+
+    transactions['transactions'] = [transaction for transaction in
+                                    transactions['transactions'] if transaction not in current_transactions]
+    for transaction in transactions['transactions']:
+        transaction['_id'] = bson.objectid.ObjectId()
+    users_collection.update_one(
+        {'_id': bson.ObjectId(user_id)},
+        {'$push': {'transactions': {'$each': transactions['transactions']}}}
+    )
+
+
+def extract_transaction_from_isracard_pdf(lines, user_id):
     data = Data()
-
-    decode_text = text.decode()
-    decode_text = remove_rtl(decode_text)
-
-    decode_text = decode_text.split('\n')
-    decode_text = list([value for value in decode_text if value != 'ה.קבע'])
-    decode_text = list([value for value in decode_text if value != ''])
-    data.credit_card_last_digits = extract_credit_card_number(decode_text)
+    lines = list([value for value in lines if value != 'ה.קבע'])
+    lines = list([value for value in lines if value != ''])
+    data.credit_card_last_digits = extract_credit_card_number(lines)
     dates = list()
     businesses = list()
 
-    for a, b in pairwise(decode_text):
+    for a, b in pairwise(lines):
         if is_timestamp(b):
             dates.append(b)
             businesses.append(a)
     dates_len = len(dates)
-    index = decode_text.index('בש"ח')
-    atms = decode_text.count('משיכת מזומנים')
-    prices = list(decode_text[index + 1: index + atms * 2: 2]) + list(decode_text[index + atms * 2 + 1: index + 1 + atms * 2 + dates_len - atms])
-    index = decode_text.index('סכו םעסקה')
+    index = lines.index('בש"ח')
+    atms = lines.count('משיכת מזומנים')
+    prices = list(lines[index + 1: index + atms * 2: 2]) + list(lines[index + atms * 2 + 1: index + 1 + atms * 2 + dates_len - atms])
+    index = lines.index('סכו םעסקה')
     start = index
     categories = list()
     while len(categories) < dates_len or index - start < dates_len:
         index += 1
-        temp = decode_text[index]
+        temp = lines[index]
         temp = temp.replace('.', '')
         if not temp.isdigit():
-            categories.append(decode_text[index])
+            categories.append(lines[index])
     transactions = {"transactions": []}
     for date, business, price, category in zip(dates, businesses, prices, categories):
         transactions['transactions'].append({
             'date': date,
-            'buissness': business,
+            'business': business,
             'price': price,
             'category': category
         })
     for transaction in transactions['transactions']:
-        transaction['_id'] = bson.objectid.ObjectId()
         date = transaction['date']
         date = date.split('/')
         date[2] = '20' + date[2]
         date = '/'.join(date)
         transaction['price'] = int(float(transaction['price']))
         transaction['date'] = datetime.datetime.strptime(date, '%d/%m/%Y')
+    add_transaction_to_user(transactions=transactions, user_id=user_id)
+
+
+def extract_transaction_from_mastercard_pdf(lines, user_id):
+    lines = list([value for value in lines if value != ''])
+    number_of_transactions = lines.index('שם בית עסק') - lines.index('תאריך רכישה') - 1
+    dates_index = lines.index('תאריך רכישה') + 1
+    business_index = lines.index('שם בית עסק') + 1
+    temp = []
+    for line in lines:
+        for number in line.split('₪'):
+            if '.' in number:
+                try:
+                    float(number)
+                    temp.append(float(number))
+                except ValueError:
+                    pass
+    temp = temp[:-1]
+    prices = temp[:number_of_transactions]
+    dates = lines[dates_index:dates_index+number_of_transactions]
+    businesses = lines[business_index:business_index+number_of_transactions]
+    categories = ['לא ידוע' for i in range(number_of_transactions)]
+    transactions = {"transactions": []}
+    for date, business, price, category in zip(dates, businesses, prices, categories):
+        transactions['transactions'].append({
+            'date': date,
+            'business': business,
+            'price': price,
+            'category': category
+        })
     for transaction in transactions['transactions']:
-        users_collection.update_one(
-            {'firstName': 'user'},
-            {'$push': {'transactions': transaction}}
-        )
+        date = transaction['date']
+        transaction['date'] = datetime.datetime.strptime(date, '%d/%m/%Y')
+    add_transaction_to_user(transactions=transactions, user_id=user_id)
+
+
+def extract_transaction_from_pdf(file_path, user_id):
+    # TODO add error message and handler for textract.exceptions.ShellError exception
+    text = textract.process(file_path, 'UTF-8')
+    decode_text = text.decode()
+    decode_text = remove_rtl(decode_text)
+    lines = decode_text.split('\n')
+    if 'להצטרפותwww.isracard.co.il :' in lines:
+        extract_transaction_from_isracard_pdf(lines, user_id)
+    else:
+        extract_transaction_from_mastercard_pdf(lines, user_id)
 
 
 if __name__ == '__main__':
